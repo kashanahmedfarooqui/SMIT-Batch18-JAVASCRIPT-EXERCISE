3f0468cb479288b0001917f05308c3cfaf7ecdd6
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-01-18 23:09:57 +0500

    Added new chapters

---
+++ javascript-exercises/chapter21-25/app .js
@@ -0,0 +1,50 @@
+// Q1
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+// var fullName = firstName + " " + lastName;
+// console.log("Hello " + fullName + "!");
+
+//Q2
+// var favoritePhone = prompt("Enter your favorite mobile phone model:");
+// console.log("My favorite phone is: " + favoritePhone);
+// console.log("Length of string: " + favoritePhone.length);
+
+// Q3
+// var country = "Pakistani";
+// var index = country.indexOf("n");
+// console.log("String: " + country);
+// console.log("Index of 'n': " + index);
+
+ // Q4
+// var greet = "Hello World";
+// var lastIndex = greet.lastIndexOf("l");
+// console.log("String: " + greet);
+// console.log("Last index of 'l': " + lastIndex);
+
+// Q5
+// var word = "Pakistani";
+// var character = word.charAt(3);
+// console.log("String: " + word);
+// console.log("Character at index 3: " + character);
+
+// Q7
+// var city = "Hyderabad";
+// var replacement = city.replace("Hyder", "Islam"); 
+// console.log("City: " + city);
+// console.log("After replacement: " + replacement);
+
+// Q9
+// var stringVal = "472";
+// var numberVal = Number(stringVal); 
+// console.log("Value: " + stringVal + " | Type: " + typeof(stringVal));
+// console.log("Value: " + numberVal + " | Type: " + typeof(numberVal));
+
+// Q10
+// var userInput = prompt("Enter some text to capitalize:");
+// console.log("Upper case: " + userInput.toUpperCase()); 
+
+// Q18
+// var str = "The quick brown fox jumps over the lazy dog";
+// var count = (str.toLowerCase().match(/the/g) || []).length;
+// console.log("Text: " + str);
+// console.log("There are " + count + " occurrence(s) of word 'the'"); 
